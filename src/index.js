1ff5ef91dd9d322bbc2af6c8e83915e44e57dc22
Emily Ford <eford@blackducksoftware.com>
2017-11-19 23:48:55 -0500

    Committing datasets/objects for game

--- src/index.js
+++ src/index.js
@@ -1,3 +1,65 @@
+var everyoneDrinks = [
+    "Drink if you went to college",
+    "Drink if you have kids",
+    "Drink if you are an aunt or uncle",
+    "Drink if you like sports",
+    "Drink if you have a pet",
+    "Drink if you have had sex in the past 48 hours",
+    "Drink if you have ever been cat called",
+    "Drink if you have two eyes",
+    "Drink if you hate Trump",
+    "Drink if you are from the East Coast",
+    "Drink if you are from the West Coast",
+    "Drink if you went to the nail salon within the past month",
+    "Drink if you are an asshole",
+    "Drink if you like musicals",
+    "Drink if play video games",
+    "Drink if you have an iPhone",
+    "Drink if you have an Android",
+    "Drink if you like tacos",
+    "Drink if you have a penis",
+    "Drink if you have boobs",
+    "Drink if you like pina cooladas and getting caught in the rain",
+    "Drink if you have ever cried watching Titanic",
+    "Drink if you have ever run over a squirrel",
+    "Drink if you have glasses",
+    "Drink if you love sushi",
+]
+
+var onePersonDrinksTrivia = [
+    "Whoever had the most recent birthday",
+    "The youngest",
+    "The oldest",
+    "The shortest",
+    "The person with longest hair",
+    "The nicest person",
+    "The biggest asshole ",
+    "The funniest person ",
+    "The nerdiest person ",
+    "The most likely to end up naked, outside their grandma's rosebush at 4am",
+    "The most likely to be a millionare ",
+    "The most athletic ",
+    "The person with the nicest butt ",
+    "The person most likely to go to bed at 9pm ",
+    "The person that is most likely to end up with 18 cats, 9 dogs and athritis ",
+    "The person who will most likely be CEO of Apple ",
+    "The person most likely to bring an escort to the mall for a date",
+    "The person most likely to streak at a sporting event",
+    "The person most likely to be president",
+    "Most likely to have a Britney Spears breakdown"
+
+]
+
+var triviaQuestions = [
+    {
+        'question' : 'question 1'
+        , 'answer' : 'test'
+    }
+];
+
+
+
+
 // Route the incoming request based on type (LaunchRequest, IntentRequest,
 // etc.) The JSON body of the request is provided in the event parameter.
 exports.handler = function (event, context) {
@@ -57,28 +119,15 @@ function onLaunch(launchRequest, session, callback) {
  */
 function onIntent(intentRequest, session, callback) {
 
-    var intent = intentRequest.intent;
-    var intentName = intent.name;
+    var intent = intentRequest;
+    var intentName = intentRequest.intent.name;
 
     handlePlayerNameResponse(intentRequest, session, callback);
-
 
     // dispatch custom intents to handlers here
     if (intentName == "PlayerNamesIntent") {
         handlePlayerNameResponse(intent, session, callback);
     }
-    /*else if (intentName === "AMAZON.YesIntent") {
-
-    } else if (intentName === "AMAZON.NoIntent") {
-
-
-    } else if (intentName === "AMAZON.StopIntent") {
-
-    } else if (intentName === "AMAZON.CancelIntent") {
-
-    } else {
-        throw "Invalid intent";
-    }*/
 }
 
 /**
@@ -93,10 +142,9 @@ function onSessionEnded(sessionEndedRequest, session) {
 
 function getWelcomeResponse(callback) {
     var speechOutput = "Get ready. If you're not drunk enough already, this game will get you drunker. Tell me" +
-        " all of the first names of the players starting with the players are...including " +
-        "if you're playing alone, you alcoholic.";
+        " how many players there are starting with the phrase there are...including if it's just you, you alcoholic.";
 
-    var reprompt = "Tell me the names, you filthy animals. ";
+    var reprompt = "Tell me the names, you filthy animals.";
 
     var header = "This game will get you drunker!";
 
@@ -111,11 +159,10 @@ function getWelcomeResponse(callback) {
 }
 
 function  handlePlayerNameResponse(intent, session, callback) {
-    console.log(intent);
+    var speechOutput = "Okay! You have " + intent.intent.slots.PlayerNumber.value + " players. Everyone shout out one " +
+        "of the numbers; whoever was first claims that number. Let's begin. Say let's play to start.";
 
-    var speechOutput = "Okay! You have " + intent + " players. Let's begin."
-
-    var reprompt = "Let's play!";
+    var reprompt = "Say let's play!";
 
     var header = "This game will get you drunker!";
 
@@ -129,6 +176,13 @@ function  handlePlayerNameResponse(intent, session, callback) {
     callback(sessionAttributes, buildSpeechletResponse(header, speechOutput, reprompt, shouldEndSession));
 }
 
+function handleLetsPlay(intent, session, callback) {
+
+}
+
+function handleTrivia(intent, session, callback) {
+
+}
 
 function handleGetHelpRequest(intent, session, callback) {
     // Ensure that session.attributes has been initialized
